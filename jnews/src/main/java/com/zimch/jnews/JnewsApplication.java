09e058a7ee8af9f66aa782c1ccbf3c130a412eff
zimch <gregoryshanin.job@gmail.com>
2023-03-02 22:14:49 +0300

    commit-project

---
+++ jnews/src/main/java/com/zimch/jnews/JnewsApplication.java
@@ -0,0 +1,13 @@
+package com.zimch.jnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnewsApplication.class, args);
+	}
+
+}
---
+++ jnews/src/test/java/com/zimch/jnews/JnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimch.jnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
